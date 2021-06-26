195ea25cac922be64a333b820315290e6fbb1aa5
Elvis <e.igiebor@cyberticks.com>
2021-06-26 08:59:23 +0100

    added jwt

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group([
+    'middlaware'=>'api',
+    'namespace'=>'App\Http\Controllers',
+    'prefix'=>'auth'
+],
+    function($router){
+        Route::post('login','AuthController'::class, 'login');
+        Route::post('register','AuthController'::class, 'register');
+        Route::post('logout','AuthController'::class, 'logout');
+        Route::get('profile','AuthController'::class, 'profile');
+        Route::post('refresh','AuthController'::class, 'refresh');
+
+    }
+);
