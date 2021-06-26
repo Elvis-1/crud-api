e9ccb7482b1182afe891cfe27f070ec2585e0de8
Elvis <e.igiebor@cyberticks.com>
2021-06-26 10:14:01 +0100

    added form request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\UserValidation;
 
 class AuthController extends Controller
 {
     public function __construct(){
         $this->middleware('auth:api',['except'=>['login','resgister']]);
     }
+
+    public function login(UserValidation $request){
+             $validator = $request->validated();
+
+            
+             if($validator->fails()){
+                 return reponse()->json($validator->errors(), 400);
+             }
+
+    }
+
+
 }
 
 
