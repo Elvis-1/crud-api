3d6d702d92d37d8a0a9e7c966865c5812e6d4a90
Elvis <e.igiebor@cyberticks.com>
2021-06-26 10:57:08 +0100

    completed auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,8 +22,70 @@ class AuthController extends Controller
                  return reponse()->json($validator->errors(), 400);
              }
 
+             $token_validity = (24*60);
+               $this->guard()->factory()->setTTL($token_validity);
+
+               if(!$token = $this->guard()->attempt($validator->validated())){
+                   return response()->json([
+                       'error'=>'Unauthorized'
+                   ], 401);
+               }
+               return $this->respondWithToken($token);
+
     }
 
+    public function register(){
+        $validator = Validator::make(
+            $request->all(),[
+                'firstName' => 'required|string|between2,100',
+                'middleName' => 'required|string|between2,100',
+                'lastName' => 'required|string|between2,100',
+                'email'=> 'required|email',
+                'password'=> 'required|confirmed|min:6',
+                'phoneNumber'=> 'required|integer',
+                'picture_url'=> 'required|string',
+                
+            ]
+        );
+        if($validator->fails()){
+            return response()->json([
+                $validator->errors()
+            ], 422);
+        }
+        $user = User::create(
+            array_merge(
+                $validator->validated(),
+                ['password'=>bycrpt($request->password)]
+            )
+        );
+        return response()->json(['message'=> 'User created successfully', 'user'=> $user]);
+    }
+
+    public function logout(){
+        $this->guard()->logout();
+
+        return response()->json(['message'=> 'User logged out successfully']);
+    }
+
+    public function profile()
+    {
+        return response()->json($this->guard()->user());
+    }
+
+
+
+    protected function respondWithToken($token){
+        return response()->json([
+            'token' => $token,
+            'token_type'=>'bearer',
+            'token_validity' => ($this->guard()->factory()->getTTL()*60),
+        ]);
+    }
+
+    public function guard(){
+        return Auth::guard();
+    }
+   
 
 }
 
